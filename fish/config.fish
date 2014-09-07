1d7652687c7cce076e5792c0a94223c21ceb931d
Ryan Sobol <contact@ryansobol.com>
2014-09-07 09:44:38 -0700

    Remove the PGDATA env variable

--- fish/config.fish
+++ fish/config.fish
@@ -2,9 +2,6 @@
 set -gx EDITOR 'atom -wd'
 set -gx ATOM_REPOS_HOME /Projects/2014/ryansobol
 
-# postgres
-set -gx PGDATA /usr/local/var/postgres
-
 # chruby
 source /usr/local/share/chruby/chruby.fish
 source /usr/local/share/chruby/auto.fish
