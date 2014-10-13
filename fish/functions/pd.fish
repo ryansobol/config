9264bbe459e81efe97eb19740591d36ef2c25354
Ryan Sobol <contact@ryansobol.com>
2014-10-13 08:51:59 -0700

    Add 2012 to searching project directories

--- fish/functions/pd.fish
+++ fish/functions/pd.fish
@@ -1,4 +1,4 @@
 function pd --description 'Change to a project directory'
-  set -l paths $HOME/Projects/{2014,2013,2014}
+  set -l paths $HOME/Projects/{2014,2013,2012}
   cd (find $paths -type d -maxdepth 2 | ack -i $argv[1] | ack -v Pods --max-count=1)
 end
