cd5202e7aa83de3cf37f1d9ea402490c357f72c8
Ryan Sobol <contact@ryansobol.com>
2015-11-03 06:57:31 -0800

    Add Docker config and remove global flag

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,11 @@
 # Atom
-set -gx EDITOR 'atom -w'
-set -gx ATOM_REPOS_HOME /Projects/2014/ryansobol
+set -x EDITOR 'atom -w'
+set -x ATOM_REPOS_HOME /Projects/2014/ryansobol
+
+# Docker
+set -x DOCKER_HOST tcp://192.168.59.103:2376
+set -x DOCKER_CERT_PATH /Users/ryansobol/.boot2docker/certs/boot2docker-vm
+set -x DOCKER_TLS_VERIFY 1
 
 # chruby
 source /usr/local/share/chruby/chruby.fish
