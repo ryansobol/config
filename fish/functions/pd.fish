51019b5aacd41e9d13ecd98ae5e634c73f34e77d
Ryan Sobol <contact@ryansobol.com>
2016-01-09 02:52:32 -0800

    Switch to -1 ack flag

--- fish/functions/pd.fish
+++ fish/functions/pd.fish
@@ -1,6 +1,6 @@
 function _find_project -a pattern
   set -l paths $HOME/Projects/{2016,2015}
-  find $paths -type d -maxdepth 2 | ack --ignore-case --max-count=1 $pattern
+  find $paths -type d -maxdepth 2 | ack --ignore-case -1 $pattern
 end
 
 function pd --description 'Change to a project directory'
