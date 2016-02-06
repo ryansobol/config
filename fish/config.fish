a9af786fbb39739df36793f035f7edf0ba6b4443
Ryan Sobol <contact@ryansobol.com>
2016-02-06 07:22:11 -0800

    Disable chruby and virtualfish

--- fish/config.fish
+++ fish/config.fish
@@ -8,9 +8,9 @@ set -x DOCKER_CERT_PATH /Users/ryansobol/.boot2docker/certs/boot2docker-vm
 set -x DOCKER_TLS_VERIFY 1
 
 # chruby
-source /usr/local/share/chruby/chruby.fish
-source /usr/local/share/chruby/auto.fish
+# source /usr/local/share/chruby/chruby.fish
+# source /usr/local/share/chruby/auto.fish
 
 # virtualfish
-source ~/.virtualfish/virtual.fish
-source ~/.virtualfish/projects.fish
+# source ~/.virtualfish/virtual.fish
+# source ~/.virtualfish/projects.fish
