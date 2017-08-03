147153d7ccf8b70c6f38fa93a78a1271fca1dd6e
Ryan Sobol <contact@ryansobol.com>
2017-08-03 16:22:04 -0700

    Add exa alias

--- fish/config.fish
+++ fish/config.fish
@@ -7,6 +7,12 @@ set -x DOCKER_HOST tcp://192.168.59.103:2376
 set -x DOCKER_CERT_PATH /Users/ryansobol/.boot2docker/certs/boot2docker-vm
 set -x DOCKER_TLS_VERIFY 1
 
+# exa
+if test -n (which exa)
+  alias ls exa
+  alias tree "exa -T"
+end
+
 # chruby
 # source /usr/local/share/chruby/chruby.fish
 # source /usr/local/share/chruby/auto.fish
