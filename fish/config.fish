b19693b4ea161e0514f8d43946f731d33ba9e8aa
Ryan Sobol <hello@ryansobol.com>
2018-08-08 10:01:13 -0700

    Remove unnecessary fish config

--- fish/config.fish
+++ fish/config.fish
@@ -2,21 +2,8 @@
 set -x EDITOR 'atom -w'
 set -x ATOM_REPOS_HOME /Projects/2014/ryansobol
 
-# Docker
-set -x DOCKER_HOST tcp://192.168.59.103:2376
-set -x DOCKER_CERT_PATH /Users/ryansobol/.boot2docker/certs/boot2docker-vm
-set -x DOCKER_TLS_VERIFY 1
-
 # exa
 if test -n (which exa)
   alias ls exa
   alias tree "exa -T"
 end
-
-# chruby
-# source /usr/local/share/chruby/chruby.fish
-# source /usr/local/share/chruby/auto.fish
-
-# virtualfish
-# source ~/.virtualfish/virtual.fish
-# source ~/.virtualfish/projects.fish
