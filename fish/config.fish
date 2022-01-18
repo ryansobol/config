39398095067eddd4d9f276d7474b034bb4ef5443
Ryan Sobol <hello@ryansobol.com>
2022-01-17 21:51:18 -0800

    Improve fzf config

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,10 @@
-# fzf and fzf.fisher
+# fzf and fzf.fish
 if command --query fzf
-  # fzf.fisher
+  # fzf.fish
   fzf_configure_bindings --directory=\cf --git_log=\cl --git_status=\cs
-
-  set fzf_fd_opts --follow --hidden --exclude=Documents --exclude=Library --exclude=.git
+  set --global fzf_fd_opts --follow --hidden --exclude=Documents --exclude=Library --exclude=.git
 
   # fzf
-  set -x FZF_DEFAULT_COMMAND "fd --type f $fzf_fd_opts"
+  set --export FZF_DEFAULT_COMMAND "fd --type f --color always $fzf_fd_opts"
+  set --export FZF_DEFAULT_OPTS '--ansi --border --height 40% --layout=reverse'
 end
