3868a0c2ae673ae23005f3a7fbbdf7ec472ace72
Ryan Sobol <hello@ryansobol.com>
2022-01-23 08:49:38 -0800

    Add colorpanels.fish plugin

---
+++ fish/functions/colorpanels.fish
@@ -0,0 +1,42 @@
+function colorpanels --description="Print a colorized panel for each color in the terminal's theme"
+  echo
+
+  set -l colors black white red yellow green cyan blue magenta
+
+  for color in $colors
+    set_color $color
+    echo -n ████
+
+    set_color "br"$color
+    echo -n ▄"  "
+  end
+
+  echo
+
+  for color in $colors
+    set_color $color
+    echo -n ████
+
+    set_color "br"$color
+    echo -n █"  "
+  end
+
+  echo
+
+  for color in $colors
+    set_color $color
+    echo -n ████
+
+    set_color "br"$color
+    echo -n █"  "
+  end
+
+  echo
+
+    for color in $colors
+    set_color "br"$color
+    echo -n " "▀▀▀▀"  "
+  end
+
+  echo
+end
