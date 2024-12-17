1a41e543abe4a4a856f8b4962a06f71b91e01c1b
Ryan Sobol <hello@ryansobol.com>
2024-12-16 16:12:02 -0800

    Remove dependency on ryansobol/bat.fish

--- fish/conf.d/bat.fish
+++
@@ -1,7 +0,0 @@
-function _bat_install --on-event bat_install
-  brew install bat
-end
-
-function _bat_uninstall --on-event bat_uninstall
-  brew uninstall bat
-end
--- fish/functions/cat.fish
+++ fish/functions/cat.fish
@@ -1,3 +1,7 @@
 function cat --description='Print the syntax-highlighted content of a collection of files to the terminal' --wraps=bat
-  bat $argv
+  if command --query bat
+    bat $argv
+  else
+    command cat $argv
+  end
 end
