78b39d5d9c0b76a6b4edb4d0512d224779a3079b
Ryan Sobol <hello@ryansobol.com>
2024-12-17 10:18:17 -0800

    Add all current cursor extensions

--- setup/07_config_cursor.fish
+++ setup/07_config_cursor.fish
@@ -36,3 +36,45 @@ for file in $cursor_user_settings_files
 	# Create the target settings symlink from the source settings file
 	ln -s $cursor_user_settings_file_source $cursor_user_settings_file_target
 end
+
+set --local cursor_extensions \
+	766b.go-outliner \
+	anysphere.pyright \
+	biomejs.biome \
+	blodwynn.featurehighlight \
+	bradlc.vscode-tailwindcss \
+	catppuccin.catppuccin-vsc \
+	catppuccin.catppuccin-vsc-icons \
+	dbaeumer.vscode-eslint \
+	editorconfig.editorconfig \
+	eriklynd.json-tools \
+	esbenp.prettier-vscode \
+	etsi0.class-collapse \
+	golang.go \
+	julienetie.vscode-template-literals \
+	kamikillerto.vscode-colorize \
+	lextudio.restructuredtext \
+	mariomatheu.syntax-project-pbxproj \
+	mechatroner.rainbow-csv \
+	meganrogge.template-string-converter \
+	ms-azuretools.vscode-docker \
+	ms-python.debugpy \
+	ms-python.python \
+	ms-python.vscode-pylance \
+	ms-vscode.atom-keybindings \
+	ms-vscode.theme-tomorrowkit \
+	nhoizey.gremlins \
+	oven.bun-vscode \
+	pkief.material-icon-theme \
+	premparihar.gotestexplorer \
+	prisma.prisma \
+	quicktype.quicktype \
+	rangav.vscode-thunder-client \
+	skyapps.fish-vscode \
+	svelte.svelte-vscode \
+	tamasfe.even-better-toml \
+	tomoki1207.pdf \
+	trixnz.go-to-method \
+	vitest.explorer \
+	yoavbls.pretty-ts-errors \
+	yzhang.markdown-all-in-one
