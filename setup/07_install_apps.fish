76c0434ba6059b6db45186fe629827f9d61e3b21
Ryan Sobol <hello@ryansobol.com>
2024-12-28 09:55:59 -0800

    Order cask apps to install alpha-numerically

--- setup/07_install_apps.fish
+++ setup/07_install_apps.fish
@@ -3,14 +3,14 @@
 # List of apps to install
 set --local apps \
 	1password \
-	steermouse \
 	cursor \
-	firefox@developer-edition \
-	google-chrome@dev \
 	elgato-camera-hub \
 	elgato-control-center \
+	firefox@developer-edition \
 	ghostty \
-	raycast
+	google-chrome@dev \
+	raycast \
+	steermouse
 
 # Get currently installed apps
 set --local installed_apps (brew list --cask)
