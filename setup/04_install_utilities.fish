7b2dc1f54938102215b291df5da939e92246af97
Ryan Sobol <hello@ryansobol.com>
2025-03-21 09:46:36 -0700

    Add 1password-cli to utilities install script

--- setup/04_install_utilities.fish
+++ setup/04_install_utilities.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/env fish
 
-set --local utils bat eza fd fzf git ripgrep
+set --local utils bat eza fd fzf git ripgrep 1password-cli
 
 for util in $utils
 	# check if a utility is NOT available in the current shell's path
